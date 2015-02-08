b32c81de867ce007b0dc82f69b022e297c59c3d2
Alec Perkins <alecperkins@gmail.com>
2015-02-07 19:30:29 -0500

    Add test for alternate usage style.

--- test/test.js
+++ test/test.js
@@ -5,3 +5,6 @@ var sampleLog = require('./sample');
 s3LogParser.parse(sampleLog, function (parsed) {
   assert.equal(parsed.length, 7);
 });
+
+// Alternate usage style.
+assert.equal(s3LogParser.parse(sampleLog).length, 7);
