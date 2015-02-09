e87f6348c773e21ab16ed9642105afceb2021383
Daniel Sarsi Orta <danielsarsi@me.com>
2015-02-09 17:09:17 -0200

    Simplify main script.
    Got rid of that multiple regex and reorganized keys positions.

--- index.js
+++ index.js
@@ -3,75 +3,62 @@
 exports.parse = function (log, callback) {
   var logs = log.split('\n');
   var parsed = [];
-  var bracketRegEx = /\[(.*?)\]/;
-  var quoteRegex = /\"(.*?)\"/;
+  var regex = /[^\s\"\[\]]+|\".*?\"|\[.*?\]/g;
 
   for (var i = 0; i < logs.length; i++) {
     var logString = logs[i];
-    if (logString.length === 0) continue;
 
-    var time = bracketRegEx.exec(logString)[1];
-    time = time.replace(/\//g, ' ');
-    time = time.replace(/:/, ' ');
-    time = new Date(time);
-    logString = logString.replace(bracketRegEx, '');
+    if (logString.length !== 0) {
+      var logStringSplit = logString.match(regex);
 
-    var requestUri = quoteRegex.exec(logString);
-    if (requestUri) {
-      requestUri = requestUri[1];
-      logString = logString.replace(quoteRegex, '');
+      var bucketOwner    = logStringSplit[0];
+      var bucket         = logStringSplit[1];
+
+      var time = logStringSplit[2];
+      time = time.replace(/[\[\]]/g, '').replace(/\//g, ' ').replace(':', ' ');
+      time = new Date(time);
+
+      var remoteIp       = logStringSplit[3];
+      var requester      = logStringSplit[4];
+      var requestId      = logStringSplit[5];
+      var operation      = logStringSplit[6];
+      var key            = logStringSplit[7];
+      var requestUri     = logStringSplit[8].substring(1, logStringSplit[8].length - 1);
+      var statusCode     = logStringSplit[9];
+      var errorCode      = logStringSplit[10];
+      var bytesSent      = logStringSplit[11];
+      var objectSize     = logStringSplit[12];
+      var totalTime      = logStringSplit[13];
+      var turnAroundTime = logStringSplit[14];
+      var referrer       = logStringSplit[15].substring(1, logStringSplit[15].length - 1);
+      var userAgent      = logStringSplit[16].substring(1, logStringSplit[16].length - 1);
+      var versionId      = logStringSplit[17];
+
+      var formatted = {
+        bucketOwner:    bucketOwner,
+        bucket:         bucket,
+        time:           time,
+        requestId:      requestId,
+        operation:      operation
+      };
+
+      if (remoteIp !== '-')       formatted['remoteIp'] = remoteIp;
+      if (requester !== '-')      formatted['requester'] = requester;
+      if (key !== '-')            formatted['key'] = key;
+      if (requestUri !== '-')     formatted['requestUri'] = requestUri;
+      if (statusCode !== '-')     formatted['statusCode'] = +statusCode;
+      if (errorCode !== '-')      formatted['errorCode'] = errorCode;
+      if (bytesSent !== '-')      formatted['bytesSent'] = +bytesSent;
+      if (objectSize !== '-')     formatted['objectSize'] = +objectSize;
+      if (totalTime !== '-')      formatted['totalTime'] = +totalTime;
+      if (turnAroundTime !== '-') formatted['turnAroundTime'] = +turnAroundTime;
+      if (referrer !== '-')       formatted['referrer'] = referrer;
+      if (userAgent !== '-')      formatted['userAgent'] = userAgent;
+      if (versionId !== '-')      formatted['versionId'] = errorCode;
+
+      parsed.push(formatted);
     }
 
-    var referrer = quoteRegex.exec(logString);
-    if (referrer) {
-      referrer = referrer[1];
-      logString = logString.replace(quoteRegex, '');
-    }
-
-    var userAgent = quoteRegex.exec(logString);
-    if (userAgent) {
-      userAgent = userAgent[1];
-      logString = logString.replace(quoteRegex, '');
-    }
-
-    var logStringSplit = logString.split(' ');
-    var bucketOwner    = logStringSplit[0];
-    var bucket         = logStringSplit[1];
-    var remoteIp       = logStringSplit[3];
-    var requester      = logStringSplit[4];
-    var requestId      = logStringSplit[5];
-    var operation      = logStringSplit[6];
-    var key            = logStringSplit[7];
-    var statusCode     = logStringSplit[9];
-    var errorCode      = logStringSplit[10];
-    var bytesSent      = logStringSplit[11];
-    var objectSize     = logStringSplit[12];
-    var totalTime      = logStringSplit[13];
-    var turnAroundTime = logStringSplit[14];
-
-    var formatted = {
-      bucketOwner:    bucketOwner,
-      bucket:         bucket,
-      time:           time,
-      requestId:      requestId,
-      operation:      operation,
-      key:            key
-    };
-
-    if (requestUri !== '-') formatted['requestUri'] = requestUri;
-    if (referrer !== '-') formatted['referrer'] = referrer;
-    if (userAgent !== '-') formatted['userAgent'] = userAgent;
-    if (remoteIp !== '-') formatted['remoteIp'] = remoteIp;
-    if (requester !== '-') formatted['requester'] = requester;
-    if (errorCode !== '-') formatted['errorCode'] = errorCode;
-
-    if (statusCode !== '-') formatted['statusCode'] = +statusCode;
-    if (objectSize !== '-') formatted['objectSize'] = +objectSize;
-    if (bytesSent !== '-') formatted['bytesSent'] = +bytesSent;
-    if (totalTime !== '-') formatted['totalTime'] = +totalTime;
-    if (turnAroundTime !== '-') formatted['turnAroundTime'] = +turnAroundTime;
-
-    parsed.push(formatted);
     if (i + 1 === logs.length) {
       if (callback) {
         callback(parsed);
